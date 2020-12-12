b588ad9247bd7030f14da716ddc338e94d803aa0
Liang Di <iamliangdi@outlook.com>
2020-12-12 11:51:53 +0800

    feat：添加网关

---
+++ wings-gateway/src/main/java/com/alone/wings/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.alone.wings.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Liang Di
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
